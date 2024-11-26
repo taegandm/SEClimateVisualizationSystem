dee359b924616b7f529b136ec798f5f6df0c0dc4
Taegan <taegandm@gmail.com>
2024-11-26 14:29:05 -0500

    Create some files for DB Connection

--- src/js/dbconnect.js
+++ src/js/dbconnect.js
@@ -3,10 +3,10 @@ const { Pool } = require('pg');
 
 // Define connection configuration
 const pool = new Pool({
-    user: 'your_username',      // Your PostgreSQL username
+    user: 'postgres',      // Your PostgreSQL username
     host: 'localhost',          // Your database server
-    database: 'your_database',  // Your database name
-    password: 'your_password',  // Your PostgreSQL password
+    database: 'gradproject',  // Your database name
+    password: '4TaeMaish!',  // Your PostgreSQL password
     port: 5432                  // Default PostgreSQL port
 });
 
